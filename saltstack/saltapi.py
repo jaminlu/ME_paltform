628dee546d6b0dacdd31eccb4e5b4768cefe19cc
carryJacky <lumingjie737@gmail.com>
2019-02-14 21:06:29 +0800

    2 commit

--- saltstack/saltapi.py
+++ saltstack/saltapi.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pycurl
-import StringIO
 import json
 import ssl
 import requests
 
-ssl._create_default_https_context = ssl._create_unverified_context
+ssl._create_default_https_context = ssl._create_unverified_context()
 
 
 class SaltApi(object):
@@ -26,12 +24,21 @@ class SaltApi(object):
 
         self.login_url = self.host + "/login"
 
+        print(self.login_url)
         resp = requests.post(
             self.login_url,
-            verify = False,
+            verify=False,
             data={
                 'username': username,
                 'password': password,
                 'eauth': eauth
             }
         )
+        if resp.status_code == 200:
+            self.header['X-Auth-Token'] = resp.json()['return'][0]['token']
+        else:
+            raise Exception('Error from source %s' % resp.text)
+
+
+if __name__ == '__main__':
+    salt = SaltApi(host='127.0.0.1', username='kbson', password='kbson')
