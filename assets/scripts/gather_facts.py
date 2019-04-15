9eeac32f03fd06afaec92f4a655ae87dfa51e9bd
able7 <lumingjie737@gmail.com>
2019-04-15 21:40:13 +0800

    add gather.py

--- assets/scripts/gather_facts.py
+++ assets/scripts/gather_facts.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+
+import sys
+import os
+import django
+
+sys.path.append("/home/root1/lmj/ME_paltform")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ManageEngine.settings")
+django.setup()
+
 from idc.models import IDC
 from utils.saltapi import SaltApi
 import sys
@@ -14,8 +23,8 @@ def gather_facts(flag="", tgt="*"):
     # idc_tel_ip = idc.tel_ip
     # idc_mob_ip = idc.mob_ip
     # idc_uni_ip = idc.uni_ip
-    salt = SaltApi(host='10.10.32.102', username='kbson', password='kbson')
-    rs = salt.salt_command(tgt, "grains.items")
+    salt = SaltApi(host='10.10.32.102', port='8000', username='kbson', password='kbson')
+    rs = salt.salt_command(tgt, "grains.item")
     print(rs)
 
 
